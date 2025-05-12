05fc86143a448d4c4e20015d51d6fc10982c6db2
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-12 20:41:27 -0300

    Feat: Criado e configurado Componente Assunto Form

---
+++ src/app/modules/assuntos/components/assunto-form/assunto-form.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Assunto } from '../../../../core/models/assunto.model';
+
+@Component({
+  selector: 'app-assunto-form',
+  standalone: true,
+  imports: [
+    CommonModule, 
+    FormsModule
+  ],
+  templateUrl: './assunto-form.component.html',
+  styleUrl: './assunto-form.component.scss'
+})
+export class AssuntoFormComponent {
+  @Input() assunto: Assunto | null = null;
+  @Output() salvo = new EventEmitter<Assunto>();
+  @Output() cancelado = new EventEmitter<void>();
+
+  assuntoModel: Assunto = {
+    id: 0,
+    descricao: ''
+  };
+
+  ngOnInit(): void {
+    if (this.assunto) {
+      this.assuntoModel = { ...this.assunto };
+    }
+  }
+
+  onSubmit(form: NgForm): void {
+    if (form.valid) {
+      this.salvo.emit(this.assuntoModel);
+    }
+  }
+
+  onCancel(): void {
+    this.cancelado.emit();
+  }
+}
