20cdaeb5f4b8ed84a51169c2fecd29726420deb7
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-12 20:31:39 -0300

    Feat: Criado Relatório Model

---
+++ src/app/core/models/relatorio.model.ts
@@ -0,0 +1,12 @@
+export interface RelatorioLivrosPorAutor {
+  autorId: number;
+  autorNome: string;
+  livroId: number;
+  livroTitulo: string;
+  editora: string;
+  edicao: number;
+  anoPublicacao: string;
+  assuntos: string;
+  precos: string;
+  totalLivros: number;
+}
