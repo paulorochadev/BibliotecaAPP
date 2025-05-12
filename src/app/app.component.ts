3a4b2e9bcda74212311f72f4216cb38dc80c13aa
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-12 20:28:16 -0300

    Feat: Configurado os arquivos do App Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingComponent } from './shared/components/loading/loading.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { NotificationComponent } from './shared/components/notification/notification.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    RouterOutlet, 
+    NavbarComponent, 
+    LoadingComponent, 
+    NotificationComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
