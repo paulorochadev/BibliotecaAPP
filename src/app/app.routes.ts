986f00f73ea6121f7307fe3c449d53dab3d29a2b
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-12 20:27:17 -0300

    Feat: Configurado as Rotas do APP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: 'livros', 
+    pathMatch: 'full' 
+  },
+  {
+    path: 'livros',
+    loadComponent: () => import('./modules/livros/livros.component').then(m => m.LivrosComponent),
+  },
+  {
+    path: 'autores',
+    loadComponent: () => import('./modules/autores/autores.component').then(m => m.AutoresComponent),
+  },
+  {
+    path: 'assuntos',
+    loadComponent: () => import('./modules/assuntos/assuntos.component').then(m => m.AssuntosComponent),
+  },
+  {
+    path: 'relatorios',
+    loadComponent: () => import('./modules/relatorios/relatorios.component').then(m => m.RelatoriosComponent),
+  },
+  { 
+    path: '**', 
+    redirectTo: 'livros' 
+  }
+];
