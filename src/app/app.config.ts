86069deaa510257c8e585454ac66aa3751e46bee
Paulo Rocha <pauloroberto_consultor@hotmail.com>
2025-05-12 20:01:35 -0300

    Chore: Config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
-
+import { errorHandlerInterceptor } from '../app/core/interceptors/error-handler.interceptor';
+import { loadingInterceptor } from '../app/core/interceptors/loading.interceptor';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([loadingInterceptor, errorHandlerInterceptor])
+    ),
+    provideClientHydration()
+  ]
 };
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { bootstrapApplication, provideClientHydration } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { errorHandlerInterceptor } from './app/core/interceptors/error-handler.interceptor';
+import { loadingInterceptor } from './app/core/interceptors/loading.interceptor';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(
+      withInterceptors([loadingInterceptor, errorHandlerInterceptor])
+    ),
+    provideClientHydration()
+  ]
+}).catch(err => console.error(err));
